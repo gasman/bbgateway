dd6050325c4147a39c079ea91ae756ce381d782c
matthew <matthew@e9b8adc4-4640-47d0-ab3f-4b6ad088a58c>
2007-05-31 00:05:08 +0000

    scrape WOS at the post level; start implementing scanner for new posts

--- fetch.rb
+++ fetch.rb
@@ -3,25 +3,57 @@
 require "models"
 require "wos_scraper"
 
-for group in WosScraper.newsgroups
-  puts "Threads in #{group[:name]}:"
-  WosScraper.threads(group[:forum_id], 1).each do |thread|
-    puts "#{thread[:sticky] ? '* ' : ''}#{thread[:id]}: #{thread[:title]}"
-  end
-  puts
+def posted_today?(crap_timestamp)
+  crap_timestamp.match(/(Hours?|Minutes?) Ago$/)
 end
 
-#forums = wos_index / "tr[td.alt1Active[a[@href*='forumdisplay.php']]]"
-#
-#forums.each do |forum|
-#  link = forum.at("td.alt1Active a[@href*='forumdisplay.php']")
-#  p link
-#  newsgroup_name = make_newsgroup_name(link.inner_text)
-##  forum_id = (link['href'].match(/f=(\d+)/)[1]).to_i
-##  if (group = Newsgroup.find_by_name(newsgroup_name))
+fresh_forums = []
+for forum in WosScraper.forums
+  if (Newsgroup.find_by_name(forum[:name]))
+    if !Article.find_by_source_post(forum[:last_post_id])
+      puts "New posts found in forum #{forum[:name]}"
+      fresh_forums << forum
+    end
+  else
+    Newsgroup.create(:name => forum[:name])
+    puts "Created newsgroup #{forum[:name]}"
+    fresh_forums << forum
+  end
+end
+
+fresh_threads = []
+for forum in fresh_forums
+  page = 1
+  seen_all_new_threads = false
+  begin
+    threads = WosScraper.threads(forum[:forum_id], page)
+    for thread in threads
+      if (!Article.find_by_source_post(thread[:last_post_id])) and posted_today?(thread[:last_post_timestamp])
+        puts "New posts found in thread '#{thread[:title]}'"
+        thread[:forum] = forum
+        fresh_threads << thread
+      elsif thread[:sticky]
+        puts "Ignoring sticky thread '#{thread[:title]}'"
+      else
+        puts "Encountered stale thread '#{thread[:title]}' - finished with #{forum[:name]}"
+        seen_all_new_threads = true
+        break
+      end
+    end
+    page += 1
+  end until seen_all_new_threads
+end
+
+
 #    puts "found newsgroup #{newsgroup_name}"
-##  else
-##    group = Newsgroup.create(:name => newsgroup_name)
-##    puts "created newsgroup #{newsgroup_name}"
-##  end
+#  puts "Threads in #{group[:name]}:"
+#  WosScraper.threads(group[:forum_id], 1).each do |thread|
+#    puts "#{thread[:sticky] ? '* ' : ''}#{thread[:id]}: #{thread[:title]}"
+#    page = WosScraper.post_page_from_post_id(thread[:last_post_id])
+#    puts "Posts on page #{page[:page_number]}:"
+#    page[:posts].each do |post|
+#      puts "  #{post[:id]}: #{post[:author]}, #{post[:crap_timestamp]}"
+#    end
+#  end
+#  puts
 #end
--- wos_scraper.rb
+++ wos_scraper.rb
@@ -9,11 +9,11 @@ module WosScraper
   end
 
   # get list of newsgroups and last post from front page
-  def WosScraper.newsgroups
+  def WosScraper.forums
     wos_index = Hpricot(open("http://www.worldofspectrum.org/forums/index.php"))
     forum_list_rows = wos_index / "table:eq(1) tbody tr"
     category = nil
-    newsgroups = []
+    forums = []
     for row in forum_list_rows
       if row % 'td.tcat'
         category = clean_string((row % "a[@href *= 'forumdisplay.php']").inner_text)
@@ -28,11 +28,11 @@ module WosScraper
         last_post_href = (row % "td:eq(2) a[img[@src *= 'lastpost']]")['href']
         last_post_id = last_post_href.match(/&amp;p=(\d+)/)[1].to_i
         forum_id = (row % "a[strong]")['href'].match(/&amp;f=(\d+)/)[1].to_i
-        newsgroups << {:name => newsgroup_name, :forum_id => forum_id, :last_post_id => last_post_id}
+        forums << {:name => newsgroup_name, :forum_id => forum_id, :last_post_id => last_post_id}
       end
     end
     
-    newsgroups
+    forums
   end
   
   def WosScraper.threads(forum_id, page)
@@ -45,11 +45,40 @@ module WosScraper
       title = title_link.inner_text
       last_post_href = (row % "a[img[@src *= 'lastpost']]")['href']
       last_post_id = last_post_href.match(/&amp;p=(\d+)/)[1].to_i
+      last_post_timestamp = (row % "td:eq(3) div > *").to_s.strip
       is_sticky = (row % "img[@src *= 'sticky']") ? true : false
-      threads << {:title => title, :id => thread_id, :last_post_id => last_post_id, :sticky => is_sticky}
+      threads << {:title => title, :id => thread_id,
+        :last_post_id => last_post_id, :last_post_timestamp => last_post_timestamp, :sticky => is_sticky}
     end
     
     threads
+  end
+  
+  def WosScraper.post_page_from_post_id(post_id)
+    page = Hpricot(open("http://www.worldofspectrum.org/forums/showthread.php?p=#{post_id}"))
+    post_page(page)
+  end
+  
+  def WosScraper.post_page_from_thread_id(thread_id, page)
+    page = Hpricot(open("http://www.worldofspectrum.org/forums/showthread.php?t=#{thread_id}&page=#{page}"))
+    post_page(page)
+  end
+
+  def WosScraper.post_page(page)
+    post_tables = page / "table.tborder[@id ^= 'post']"
+    posts = []
+    for tbl in post_tables
+      post_id = tbl['id'].match(/^post(\d+)$/)[1].to_i
+      crap_timestamp = (tbl % "td.thead").inner_text.strip
+      author = (tbl % ".bigusername").inner_text
+      body = tbl % "div[@id ^= 'post_message_']"
+      sig = tbl % "div[@id ^= 'post_message_'] ~ div"
+      posts << {:id => post_id, :crap_timestamp => crap_timestamp, :author => author, :body => body, :sig => sig}
+    end
+    page_number_block = page % "span[@title ^= 'Showing results'] strong"
+    page_number = page_number_block.nil? ? 1 : page_number_block.inner_text.to_i
+    
+    { :page_number => page_number, :posts => posts }
   end
 
 end
