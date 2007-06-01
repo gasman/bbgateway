8cbe3216285da7bc39862582e174fb066fe5dd7f
matthew <matthew@e9b8adc4-4640-47d0-ab3f-4b6ad088a58c>
2007-06-01 20:49:44 +0000

    handle links and italics (and another smiley)

--- usenet_format.rb
+++ usenet_format.rb
@@ -1,7 +1,8 @@
 module UsenetFormat
 
   SMILEYS = {
-    'images/smilies/icon_smile.gif' => ':)'
+    'images/smilies/icon_smile.gif' => ':)',
+    'images/smilies/icon_biggrin.gif' => ':-D'
   }
 
   # adapted from http://macromates.com/blog/2006/wrapping-text-with-regular-expressions/
@@ -24,8 +25,15 @@ module UsenetFormat
           SMILEYS[i['src']] || "(Image: #{i['src']})"
         elsif i.name == 'a'
           link_ref = links.size
-          links << i['href']
-          "#{inlines_to_text(i.children, links)} [#{link_ref}]"
+          linktext = inlines_to_text(i.children, links)
+          if linktext == i['href']
+            linktext
+          else
+            links << i['href']
+            "#{linktext} [#{link_ref}]"
+          end
+        elsif i.name == 'i'
+          "/#{inlines_to_text(i.children, links)}/"
         else
           i.to_s # output raw HTML
         end
@@ -48,6 +56,7 @@ module UsenetFormat
           text += render_inlines(inlines, quote_level, links)
           inlines = []
         elsif node.name == 'div'
+          next if node.classes.include?('smallfont')
           text += render_inlines(inlines, quote_level, links)
           inlines = []
           text += clean_html_traverse(node, quote_level, links)
@@ -57,7 +66,7 @@ module UsenetFormat
           divs = node / "/tr/td/div"
           if divs.size == 2
             text += "#{(divs[0] % 'strong').inner_text} wrote:\n"
-            text += clean_html_traverse(divs[1], quote_level + 1, links) + "\n"
+            text += clean_html_traverse(divs[1], quote_level + 1, links)
           else
             text += clean_html_traverse(node, quote_level, links)
           end
@@ -73,7 +82,16 @@ module UsenetFormat
   end
   
   def UsenetFormat.clean_html(doc)
-    clean_html_traverse(doc, 0, [])
+    links = []
+    txt = clean_html_traverse(doc, 0, links)
+    txt.sub!(/^\n+/, '')
+    unless links.empty?
+      txt += "\n"
+      links.each_with_index do |url, i|
+        txt += "[#{i}] #{url}\n"
+      end
+    end
+    txt
   end
   
 end
