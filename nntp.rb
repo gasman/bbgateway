55d49e181f53a104820992a6f2897b1c457db92a
matthew <matthew@e9b8adc4-4640-47d0-ab3f-4b6ad088a58c>
2007-06-02 12:07:22 +0000

    proper command line parsing for nntp.rb

--- nntp.rb
+++ nntp.rb
@@ -1,10 +1,29 @@
 #!/usr/local/bin/ruby
 
 require 'nntp_server'
+require 'optparse'
 
-port = ARGV.shift || 119
-host = ARGV.shift # default is to bind everything
+nntp_options = {
+  :port => 119,
+  :host => ''
+}
 
-s = NNTPServer.new(:port => port, :host => host)
+opts = OptionParser.new do |opts|
+  # Cast 'delay' argument to a Float.
+  opts.on("-p", "--port N", Integer, "Port number to listen on (default 119)") do |n|
+    nntp_options[:port] = n
+  end
+
+  opts.on("-h", "--host [IP]", String, "Host to bind to (default all)") do |h|
+    nntp_options[:host] = h
+  end
+
+  opts.on_tail("-?", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+end.parse!
+
+s = NNTPServer.new(nntp_options)
 
 s.start
